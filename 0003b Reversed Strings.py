52fec0facf915f031c651efad5e30bc18ac1ac17
John Kim <johnyeekim@gmail.com>
2020-07-23 04:24:12 -0500

    add kata 0003

---
+++ 0003b Reversed Strings.py
@@ -0,0 +1,13 @@
+##Complete the solution so that it reverses the string passed into it.
+##
+##'world'  =>  'dlrow'
+
+def solution(string):
+    # Pythonic way :)
+    return string[::-1]
+    
+    # For beginners it's good practise 
+    # to know how reverse() or [::-1]
+    # works on the surface
+    #for char in range(len(string)-1,-1,-1):
+        #return string[char]
